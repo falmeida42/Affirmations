5bf33a6cc86611d80b80e4ec807fcec06168b64c
falmeida <flaviomslima@gmail.com>
2021-10-04 09:10:33 -0300

    Affirmations 04/10/2021 List of strings in RecycleView added

--- app/src/main/java/com/falmeida/affirmations/MainActivity.kt
+++ app/src/main/java/com/falmeida/affirmations/MainActivity.kt
@@ -2,10 +2,22 @@ package com.falmeida.affirmations
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.falmeida.affirmations.adapter.ItemAdapter
+import com.falmeida.affirmations.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Initialize Data
+        val myDataset = Datasource().loadAffirmations()
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+
+        //Improve performance
+        recyclerView.setHasFixedSize(true)
     }
 }
